1903e44561b551a235be166078a78f566fc189a1
Marty <martin.cornu43@gmail.com>
2021-02-17 22:14:34 +0100

    press to start

--- snake.py
+++ snake.py
@@ -17,15 +17,13 @@ food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
 my_turtle = Turtle()
-g_loose = 0
-g_restart = 0
+g_start = 0
 
-def restart():
-    global g_restart
-    global g_loose
-    if g_loose == 1:
-        print('restart function')
-        g_restart = 1
+def start():
+    global g_start
+    if g_start == 0:
+        g_start = 1
+        my_turtle.undo()
 
 def change(x, y):
     "Change snake direction."
@@ -37,58 +35,56 @@ def inside(head):
     return -200 < head.x < 190 and -200 < head.y < 190
 
 def move():
-    global g_loose
-    global g_restart
+    global g_start
     
-    "Move snake forward one segment."
-    head = snake[-1].copy()
-    head.move(aim)
+    if g_start == 1:
+        "Move snake forward one segment."
+        head = snake[-1].copy()
+        head.move(aim)
 
-    if not inside(head) or head in snake:
-        square(head.x, head.y, 9, 'red')
+        if not inside(head) or head in snake:
+            square(head.x, head.y, 9, 'red')
+            update()
+            my_turtle.write("GAMEOVER!", font=("Arial", 16, "bold"), align="center")
+            return
+
+        snake.append(head)
+
+        if head == food:
+            print('Snake:', len(snake))
+            food.x = randrange(-15, 15) * 10
+            food.y = randrange(-15, 15) * 10
+        else:
+            snake.pop(0)
+        #display code and set an output to high if len > ...
+            if len(snake) > 1:
+                print('Win!')
+                clear()
+                my_turtle.color('green')
+                my_turtle.write("WIN! CODE : 43120", font=("Arial", 25, "bold"), align="center")
+                return
+
+        clear()
+
+        for body in snake:
+            square(body.x, body.y, 9, 'black')
+
+        square(food.x, food.y, 9, 'green')
         update()
-        my_turtle.write("GAMEOVER! Press any key to restart", font=("Arial", 16, "bold"), align="center")
-        g_loose = 1
-        #while True: does not work with it
-        if g_restart == 1:
-            print('lets restart')
-            g_restart = 0
-            g_loose = 0
-            #break
 
-    snake.append(head)
-
-    if head == food:
-        print('Snake:', len(snake))
-        food.x = randrange(-15, 15) * 10
-        food.y = randrange(-15, 15) * 10
-    else:
-        snake.pop(0)
-
-    #display code and set an output to high if len > ...
-        if len(snake) > 1:
-            print('Win!')
-            my_turtle.color('green')
-            my_turtle.write("WIN! CODE : 43120", font=("Arial", 25, "bold"), align="center")
-
-    clear()
-
-    for body in snake:
-        square(body.x, body.y, 9, 'black')
-
-    square(food.x, food.y, 9, 'green')
-    update()
     ontimer(move, 100)
+    
 
 setup(420, 420, 370, 0)
 hideturtle()
 my_turtle.hideturtle()
 tracer(False)
 listen()
+onkey(lambda: start(), 'space')
 onkey(lambda: change(10, 0), 'Right')
 onkey(lambda: change(-10, 0), 'Left')
 onkey(lambda: change(0, 10), 'Up')
 onkey(lambda: change(0, -10), 'Down')
-onkey(lambda: restart(), 'space')
+my_turtle.write("Press space to start", font=("Arial", 16, "bold"), align="center")
 move()
 done()
